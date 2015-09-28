4282cb92d33801271624e6710e3bb6194bf87753
fpapinea <francispapineau@gmail.com>
2015-09-28 13:58:36 -0400

    Started looking at the structure of the entities

---
+++ NetworkMonitor/Probe/Probes/MacProbe.py
@@ -0,0 +1,48 @@
+"""
+
+    :MacProbe:
+    ==========
+
+    :
+    This is the mac address poller. It scans the network and
+    gets the device mac addresses. Once the addresses are
+    validated and cached, they are entered in the resource manager
+    where other processes can use them for alerts and correlation.
+    :
+
+    :copyright: (c) 9/28/2015 by fpapinea.
+    :license: BSD, see LICENSE for more details.
+
+    Author:         gammaRay
+    Version:        :1.0:
+    Date:           9/28/2015
+"""
+
+"""
+=============================================
+Imports
+=============================================
+"""
+
+"""
+=============================================
+Constants
+=============================================
+"""
+
+# Program Attributes
+__author__  =   "gammaRay"
+__version__ =   "1.0"
+__date__    =   "9/28/2015"
+
+"""
+=============================================
+Variables
+=============================================
+"""
+
+"""
+=============================================
+Source
+=============================================
+"""
