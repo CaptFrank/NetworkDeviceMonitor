2c5fad1368ebc0161f316758cdc040d94e6f14a9
fpapinea <francispapineau@gmail.com>
2015-10-01 16:24:01 -0400

    Started looking at the structure of the entities

--- NetworkMonitor/Storage/Tables/Persistent/Access.py
+++ NetworkMonitor/Storage/Tables/Persistent/Access.py
@@ -3,7 +3,10 @@
     :Access:
     ==========
 
-    :description:
+    :
+    This is the module that defines access tokens,
+    either unknown or known access tokens.
+    :
 
     :copyright: (c) 15-09-30 by francispapineau.
     :license: BSD, see LICENSE for more details.
@@ -20,6 +23,8 @@ Imports
 =============================================
 """
 
+from mongoengine import *
+
 """
 =============================================
 Constants
@@ -27,18 +32,50 @@ Constants
 """
 
 # Program Attributes
-__author__ = "francispapineau"
-__version__ = ""  # TODO
-__date__ = "15-09-30"
-
-"""
-=============================================
-Variables
-=============================================
-"""
+__author__  =   "gammaRay"
+__version__ =   "1.0"
+__date__    =   "9/28/2015"
 
 """
 =============================================
 Source
 =============================================
 """
+
+class Access(Document):
+    """
+    This is the overall accessing document that holds
+    both valid accesses and invalid accesses within the network.
+    """
+
+    # Database Access Name
+    name        = StringField(
+        required=True
+    )
+
+    # The db uuid
+    uuid        = UUIDField(
+        required=True
+    )
+
+    # Date created
+    created     = DateTimeField(
+        required=True
+    )
+
+    # Date updated
+    updated     = DateTimeField()
+
+    # Metadata
+    meta        = {
+        'allow_inheritance'     : True,
+        'collection'            : 'access',
+
+        'indexes'               : [
+            '#name',
+            '#uuid',
+            '#created',
+            '#updated'
+        ]
+
+    }
