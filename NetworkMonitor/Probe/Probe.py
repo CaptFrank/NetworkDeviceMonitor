8b109d96ef31984f6898f1b0ff5deb43ffce5835
fpapinea <francispapineau@gmail.com>
2015-12-02 14:05:42 -0500

    Started Logstash Interface

--- NetworkMonitor/Probe/Probe.py
+++ NetworkMonitor/Probe/Probe.py
@@ -41,7 +41,7 @@ __author__  =   "gammaRay"
 __version__ =   "1.0"
 __date__    =   "9/28/2015"
 
-PLACEHOLDER = ""
+PLACEHOLDER =   ""
 
 """
 =============================================
--- NetworkMonitor/Probe/Probes/Network/PortProbe.py
+++ NetworkMonitor/Probe/Probes/Network/PortProbe.py
@@ -1,15 +1,19 @@
 """
 
-    :name:
+    :PortProbe:
     ==========
 
-    :description:
+    :
+    This is a probe that will scan for available ports on a
+    device. This probe will use the nmap binary to ping each
+    port.
+    :
 
     :copyright: (c) 10/23/2015 by fpapinea.
     :license: BSD, see LICENSE for more details.
 
     Author:         fpapinea
-    Version:        :version: #TODO
+    Version:        :1.0:
     Date:           10/23/2015
 """
 
@@ -19,6 +23,12 @@ Imports
 =============================================
 """
 
+import gc
+import nmap
+import psutil
+from NetworkMonitor.Probe.Probe \
+    import Probe, PLACEHOLDER
+
 """
 =============================================
 Constants
@@ -26,18 +36,64 @@ Constants
 """
 
 # Program Attributes
-__author__ = 'fpapinea'
-__version__ = ""  #TODO
-__date__ = "10/23/2015"
-
-"""
-=============================================
-Variables
-=============================================
-"""
+__author__  =   "gammaRay"
+__version__ =   "1.0"
+__date__    =   "9/28/2015"
 
 """
 =============================================
 Source
 =============================================
 """
+
+class PortProbe(Probe):
+    """
+    This is the user probe that scans what ports are on
+    a specific system.
+
+    extends: Probe
+    """
+
+    # The class name
+    name        = "PortProbe"
+
+    # The probe type
+    type        = "Ports"
+
+    # Description
+    description = \
+    "Gets the ports on the probe that is running."
+
+    # Fields for filtering
+    fields      = []
+
+    # Groups
+    groups      = [
+        "ports",
+        "prefetch",
+        "reconnaissance"
+    ]
+
+    # Definition
+    definition  = {}
+
+    # Template
+    template    = {}
+
+    # Data
+    data        = {}
+
+    # Continuous flag
+    continuous  = False
+
+    def __init__(self, queue):
+
+        return
+
+    def setup(self):
+
+        return
+
+    def _run(self):
+
+        return
