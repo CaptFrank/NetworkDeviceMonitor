35be71a97e6fb7fefc702ba3b1ee23f59e1cad73
fpapinea <francispapineau@gmail.com>
2016-01-06 12:34:32 -0500

    Added Probe local database

---
+++ NetworkMonitor/Probe/Probes/Active/Host/ProcessProbe.py
@@ -0,0 +1,42 @@
+"""
+
+    :ProcessProbe:
+    ==========
+
+    :
+    This is the active process probe that is meant to be interactive.
+    Through this probe, if set to root privileges, the user will
+    be able to suspends, kill, and start processes on the host that
+    is running this framework.
+    :
+
+    :copyright: (c) 10/23/2015 by gammaRay.
+    :license: BSD, see LICENSE for more details.
+
+    Author:         gammaRay
+    Version:        :1.0:
+    Date:           10/23/2015
+"""
+
+"""
+=============================================
+Imports
+=============================================
+"""
+
+"""
+=============================================
+Constants
+=============================================
+"""
+
+# Program Attributes
+__author__  =   "gammaRay"
+__version__ =   "1.0"
+__date__    =   "9/28/2015"
+
+"""
+=============================================
+Source
+=============================================
+"""
